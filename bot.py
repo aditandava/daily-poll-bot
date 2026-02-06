04fa8c85c80e468207bdb70342f78fb7b94fa51b
bhargab bikash khataniar <aditandava@gmail.com>
2026-02-06 22:26:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -219,7 +219,7 @@ def get_unique_motivation():
         try:
             url = "https://api.groq.com/openai/v1/chat/completions"
             headers = {"Authorization": f"Bearer {GROQ_API_KEY}", "Content-Type": "application/json"}
-            prompt = "Write exactly ONE savage, short, high-intensity study command. Max 15 words. No hashtags. No quotes. Output ONLY the text."
+            prompt = "Write exactly ONE savage, short, high-intensity study motivation . Max 15 words. No hashtags. No quotes. Output ONLY the text. you can use hindi too ."
             payload = {"model": "llama-3.1-8b-instant", "messages": [{"role": "user", "content": prompt}], "temperature": 1.1}
             
             response = requests.post(url, headers=headers, json=payload, timeout=5)
